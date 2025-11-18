547f6e6a7eb5f2d1c90bb23859b50b6db181a4b3
Francis Hineman <65574457+francishineman@users.noreply.github.com>
2025-11-18 14:21:43 -0800

    Update reduce_function.py

--- functions/reduce_function.py
+++ functions/reduce_function.py
@@ -1,1 +1,11 @@
+from functools import reduce
 
+sum_of_numbers = lambda x, y: x+y
+
+list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+output = []
+
+output = reduce(sum_of_numbers, list_of_numbers)
+
+print(output)
