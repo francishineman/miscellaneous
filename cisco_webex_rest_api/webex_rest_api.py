022b0c04fd1811b208005d688823ebdcbb08229d
Francis Hineman <65574457+francishineman@users.noreply.github.com>
2025-12-03 21:38:43 -0800

    Update webex_rest_api.py

--- cisco_webex_rest_api/webex_rest_api.py
+++ cisco_webex_rest_api/webex_rest_api.py
@@ -1,1 +1,15 @@
+import requests
+import json
 
+# Get token from: https://developer.webex.com/calling/docs/getting-started
+
+WEBEX_API_URL = "https://webexapis.com/v1/rooms"
+WEBEX_ACCESS_TOKEN = "GET_TOKEN_FROM"
+
+httpHeaders = {'Authorization': 'Bearer ' + WEBEX_ACCESS_TOKEN}
+
+response = requests.get(url=WEBEX_API_URL, headers=httpHeaders)
+
+print(response.status_code)
+
+print(json.dumps(response.json()))
