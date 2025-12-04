80a1f4027d806aad60c59595d27edbab5116f011
Francis Hineman <65574457+francishineman@users.noreply.github.com>
2025-12-04 13:43:16 -0800

    Update webex_rest_api.py

--- cisco_webex_rest_api/webex_rest_api.py
+++ cisco_webex_rest_api/webex_rest_api.py
@@ -1,14 +1,27 @@
 import requests
 import json
+import os
 
-# Get token from: https://developer.webex.com/calling/docs/getting-started
+
+# TOKEN = 'Get token from: https://developer.webex.com/calling/docs/getting-started'
+# This TOKEN is good for 12 hours. After that, you have to get a new token.
 
 WEBEX_API_URL = "https://webexapis.com/v1/rooms"
-WEBEX_ACCESS_TOKEN = "GET_TOKEN_FROM"
+WEBEX_ACCESS_TOKEN = TOKEN
 
-httpHeaders = {'Authorization': 'Bearer ' + WEBEX_ACCESS_TOKEN}
+HEADERS = {
+"Authorization": f"Bearer {os.getenv('WEBEX_API_TOKEN')}",
+"Content-Type": "application/json"
+}
 
-response = requests.get(url=WEBEX_API_URL, headers=httpHeaders)
+PAYLOAD = {
+"name": "DevNet Associate Certification Team"
+}
+
+# response = requests.get(url=WEBEX_API_URL, headers=httpHeaders)
+response = requests.request("POST", WEBEX_API_URL, data=json.dumps(PAYLOAD), headers=HEADERS)
+
+print(response.text)
 
 print(response.status_code)
 
