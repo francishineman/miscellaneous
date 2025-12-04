21503b6676aa893705e1b458afac04c278ed644b
Francis Hineman <65574457+francishineman@users.noreply.github.com>
2025-12-04 14:35:21 -0800

    Update webex_rest_api.py
    Expand Headers and payload. Debugged issue with name/title in the Payload.

--- cisco_webex_rest_api/webex_rest_api.py
+++ cisco_webex_rest_api/webex_rest_api.py
@@ -1,28 +1,21 @@
 import requests
 import json
-import os
-
 
 # TOKEN = 'Get token from: https://developer.webex.com/calling/docs/getting-started'
 # This TOKEN is good for 12 hours. After that, you have to get a new token.
-
+WEBEX_API_TOKEN = "YOUR TOKEN HERE"
 WEBEX_API_URL = "https://webexapis.com/v1/rooms"
-WEBEX_ACCESS_TOKEN = TOKEN
 
 HEADERS = {
-"Authorization": f"Bearer {os.getenv('WEBEX_API_TOKEN')}",
-"Content-Type": "application/json"
+    "Authorization": f"Bearer {WEBEX_API_TOKEN}",
+    "Content-Type": "application/json"
 }
 
 PAYLOAD = {
-"name": "DevNet Associate Certification Team"
+    "title": "Performance Test, Team Meeting"
 }
 
-# response = requests.get(url=WEBEX_API_URL, headers=httpHeaders)
 response = requests.request("POST", WEBEX_API_URL, data=json.dumps(PAYLOAD), headers=HEADERS)
 
-print(response.text)
-
-print(response.status_code)
-
-print(json.dumps(response.json()))
+print(f"\nHTTP Status Code: {response.status_code}")
+print(json.dumps(response.json(), indent=4))
